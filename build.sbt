40a2d726bda4fe4e9046ecdaa6158c665998f1b1
Hiroshi Yamaguchi <sanberg12@gmail.com>
2017-02-17 00:48:59 +0900

    version ups

--- build.sbt
+++ build.sbt
@@ -5,7 +5,7 @@ lazy val root = (project in file(".")).
   settings(
     inThisBuild(List(
       organization := "com.example",
-      scalaVersion := "2.12.0",
+      scalaVersion := "2.12.1",
       scalaJSStage := FullOptStage
     )),
     name := "Scala.js study",
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import org.scalajs.sbtplugin.ScalaJSPlugin
 import org.scalajs.sbtplugin.ScalaJSPlugin.autoImport._
 
 object Dependencies {
-  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.0"
+  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.1"
   lazy val scalaJSDom = Def.setting("org.scala-js" %%% "scalajs-dom" % "0.9.1")
   lazy val scalaJSJQuery = Def.setting("be.doeraene" %%% "scalajs-jquery" % "0.9.1")
   lazy val webJarJQuery = Def.setting("org.webjars" % "jquery" % "2.1.4" / "2.1.4/jquery.js")
